b69c9a02dafe35fe2ebd492f2f97d15a05b54141
Pranav Jain <the.pranav.jain.1@gmail.com>
2024-12-05 13:47:30 -0500

    replaced topojson with geojson

--- Website/lactose_intolerance_proj/app/page.tsx
+++ Website/lactose_intolerance_proj/app/page.tsx
@@ -3,20 +3,17 @@
 import React, { useEffect, useRef } from 'react';
 import * as d3 from 'd3';
 import * as topojson from 'topojson-client'
+import ancientHumanData from "./ancientHumanData.json"
 
 // json file from here:
-// https://github.com/topojson/world-atlas?tab=readme-ov-file
-import world from "./countries-110m.json"
-
-import ancient_human_data from "./ancientHumanData.json"
-
-console.log(ancient_human_data)
-
+// https://geojson-maps.kyd.au/
+import geoJson from "./worldMap.json"
 
 // Used the following tutorials when designing the d3 visualization:
 // - https://clouddevs.com/next/data-visualization-with-d3js/
 // - https://www.youtube.com/watch?v=hrJ64jpYb0A&t
 // - https://observablehq.com/@d3/world-map-svg
+// - https://www.d3indepth.com/datajoins/
 
 function AncientDnaMap() {
   const svgRef = useRef();
@@ -26,34 +23,31 @@ function AncientDnaMap() {
     const width = 975;
     const height = 610;
 
-    const projection= d3.geoNaturalEarth1()
-
-    const path = d3.geoPath(d3.geoNaturalEarth1())
+    const projection = d3.geoNaturalEarth1()
+    const geoGenerator = d3.geoPath(projection)
     const svg = d3.select(svgRef.current)
       .append('svg')
       .attr('width', width)
       .attr('height', height);
 
-    const worldBackground = svg.append('path')
+    const world  = svg.selectAll('path')
+      .data(geoJson.features)
+      .join('path')
+      .attr('d', geoGenerator)
       .attr('fill', '#ddd')
-      .attr('d', path(topojson.feature(world, world.objects.land)))
-
-    const worldBorders = svg.append('path')
-      .attr('fill', 'none')
-      .attr('stroke', '#fff')
+      .attr('stroke', '#000')
+      .attr('stroke-width', '0.5')
       .attr('stroke-linejoin', 'round')
       .attr('stroke-linecap', 'round')
-      .attr('d', path(topojson.feature(world, world.objects.countries)))
 
-    const ancientHumanDataElements = svg.selectAll('g')
-      .data(ancient_human_data["elements"])
-      .join('g');
-    
-    ancientHumanDataElements.append('g')
-      .attr('transform', d => `translate(${projection([d.Longitude, d.Latitude]).join(",")})`)
-      .append('circle')
-    .attr('r', 0.5)
-    
+    const ancientHumanDataElements = svg.selectAll('circle')
+      .data(ancientHumanData["elements"])
+      .join('circle')
+      .attr('r', 1)
+      .attr('fill', '#f00')
+      .attr('cx', d => projection([d.Longitude, d.Latitude])[0])
+      .attr('cy', d => projection([d.Longitude, d.Latitude])[1])
+
   }, []);
 
   return (
